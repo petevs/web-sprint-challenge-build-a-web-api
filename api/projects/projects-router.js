15e6a2a127e9e42062ac1d898cc0c7f6abf11fe3
Pete VS <pete_vs88@me.com>
2021-02-20 10:00:10 -0500

    basic projects router completed

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,10 +4,44 @@ const projects = require('./projects-model')
 
 const router = express.Router()
 
-router.get('/', ( req, res ) => {
-    res.status(200).json({
-        message: "projects router hooked up"
-    })
+router.get('/', ( req, res, next ) => {
+    projects.get()
+        .then((projects) => {
+            res.status(200).json(projects)
+        })
+        .catch(next)
+})
+
+router.get('/:id', ( req, res, next ) => {
+    projects.get(req.params.id)
+        .then((projects) => {
+            res.status(200).json(projects)
+        })
+        .catch(next)
+})
+
+router.post('/', ( req, res, next ) => {
+    projects.insert(req.body)
+        .then((project) => {
+            res.status(200).json(project)
+        })
+        .catch(next)
+})
+
+router.put('/:id', ( req, res, next ) => {
+    projects.update(req.params.id, req.body)
+        .then( (project) => {
+            res.status(200).json(project)
+        })
+        .catch(next)
+})
+
+router.delete('/:id', ( req, res, next ) => {
+    projects.remove(req.params.id)
+        .then((project) => {
+            res.status(200).json(project)
+        })
+        .catch(next)
 })
 
 module.exports = router
