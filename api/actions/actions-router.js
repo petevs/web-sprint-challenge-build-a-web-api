6a5f55db1597b8074985266bef42caeb91706366
Pete VS <pete_vs88@me.com>
2021-02-20 09:36:30 -0500

    added action routers no error handling yet

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,9 +5,38 @@ const actions  = require('./actions-model')
 const router = express.Router()
 
 router.get('/', ( req, res ) => {
-    res.status(200).json({
-        message: "hi I am working"
-    })
+    actions.get()
+        .then((actions) => {
+            res.status(200).json(actions)
+        })
+})
+
+router.get('/:id', ( req, res ) => {
+    actions.get(req.params.id)
+        .then((actions) => {
+            res.status(200).json(actions)
+        })
+})
+
+router.post('/', ( req, res) => {
+    actions.insert(req.body)
+        .then((action) => {
+            res.status(200).json(action)
+        })
+})
+
+router.put('/:id', ( req, res) => {
+    actions.update(req.params.id, req.body)
+        .then((action) => {
+            res.status(200).json(action)
+        })
+})
+
+router.delete('/:id', ( req, res ) => {
+    actions.remove(req.params.id)
+        .then( (action) => {
+            res.status(200).json(action)
+        })
 })
 
 module.exports = router
