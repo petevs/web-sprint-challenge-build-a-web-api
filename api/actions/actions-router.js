bc89cd38402c4b93efb29f9db63a56fe078e170f
Pete VS <pete_vs88@me.com>
2021-02-20 09:25:47 -0500

    action router hooked up to server

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,13 @@
 // Write your "actions" router here!
+const express = require('express')
+const actions  = require('./actions-model')
+
+const router = express.Router()
+
+router.get('/', ( req, res ) => {
+    res.status(200).json({
+        message: "hi I am working"
+    })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const server = express();
+const actionsRouter = require('./actions/actions-router')
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+server.use(express.json())
+server.use('/api', actionsRouter)
+
 
 module.exports = server;
