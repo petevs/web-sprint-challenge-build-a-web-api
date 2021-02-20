6e0945f195cdbe9cb6efba14f471d41d77c07278
Pete VS <pete_vs88@me.com>
2021-02-20 10:37:02 -0500

    stretch delpy to heroku

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ Go code!
 
 const server = require("./api/server")
 
-const port = 4000
+const port = process.env.PORT || 4000
 
 server.listen(port, () => {
     console.log(`server running at http://localhost:${port}`)
