8c7e9be13a29ff0ef1e5ee28138a59f2c56bd6d9
Pete VS <pete_vs88@me.com>
2021-02-20 09:51:30 -0500

    connected projects router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,13 @@
 // Write your "projects" router here!
+const express = require('express')
+const projects = require('./projects-model')
+
+const router = express.Router()
+
+router.get('/', ( req, res ) => {
+    res.status(200).json({
+        message: "projects router hooked up"
+    })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const server = express();
 const actionsRouter = require('./actions/actions-router')
+const projectsRouter = require('./projects/projects-router')
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 server.use(express.json())
-server.use('/api', actionsRouter)
+server.use('/api/actions', actionsRouter)
+server.use('/api/projects', projectsRouter)
 server.use((err, req, res, next) => {
     console.log(err)
     res.status(500).json({
